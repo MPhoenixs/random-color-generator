e5e5eae3c0f113ebd9725f2e2fd6488fdb565a93
MPhoenixs <mayankluffy1234@gmail.com>
2025-07-18 11:18:43 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const getcolor = ()=>{
+    const randomnumber = Math.floor(Math.random()*16777215);
+    const hexcode = "#" + randomnumber.toString(16);
+    document.body.style.backgroundColor = hexcode;
+    document.getElementById("code").innerText = hexcode;
+    navigator.clipboard.writeText(hexcode);
+}
+document.getElementById("btn").addEventListener(
+    "click",getcolor
+);
+getcolor();
